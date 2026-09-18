340064803ed197a300e48efdc9cc391fa3d92e08
gleboss571 <119451098+gleboss571@users.noreply.github.com>
2026-09-18 19:02:33 +0300

    Update combo1.lua

--- combo1.lua
+++ combo1.lua
@@ -10,7 +10,7 @@ local Workspace         = game:GetService("Workspace")
 -- ====================== НАСТРОЙКИ ======================
 local FIREBASE_URL        = "https://combo-6dd8a-default-rtdb.europe-west1.firebasedatabase.app"
 local ACCOUNT_ID          = 1
-local TOTAL_ACCOUNTS      = 4
+local TOTAL_ACCOUNTS      = 3
 local START_DELAY         = 10
 local COMBO_DELAY         = 17
 local CYCLE_COUNT         = 9
